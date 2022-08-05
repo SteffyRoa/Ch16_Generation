145ba4fc945c5bf9c13b75b15ba8f051f999a13e
Steffy <85137930+SteffyRoa@users.noreply.github.com>
2022-08-05 22:26:04 +0100

    test

---
+++ JS-04Ejercicios/main.js
@@ -0,0 +1,23 @@
+// Ejercicio 1 
+
+let filaBanco = ["Sofia", "David", "Juan"];
+console.log(filaBanco);
+
+filaBanco.push ("Sara", "Agustin");
+console.log(filaBanco);
+
+filaBanco.shift();
+console.log(filaBanco);
+
+filaBanco.splice(1,0, "Renata");
+console.log(filaBanco);
+
+filaBanco.push ("Elena")
+console.log(filaBanco);
+
+//ejercicio 2
+let imprimir ="*";
+for(let i=0; i<5;i++){
+    console.log("\t",imprimir);
+    imprimir = imprimir+" *";
+}
