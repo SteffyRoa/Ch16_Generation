6a47bb20803c362028f29d76d79ae7c3dc10cabc
Steffy <85137930+SteffyRoa@users.noreply.github.com>
2022-08-09 20:51:35 +0100

    avance api

---
+++ JS-07/main.js
@@ -0,0 +1,31 @@
+
+
+const urlAleatorios = "https://dog.ceo/api/breeds/image/random"
+/*Elementos del DOM*/
+
+const imagenPerrito =document.getElementById("img-perrito");
+
+const btn = document.getElementById("btn-perrito")
+console.log(btn)
+
+/*Eventos*/
+btn.addEventListener("click", () => {
+    console.log("Boton presionado");
+    obtenerPerritoAleatorio(urlAleatorios)
+    
+})
+
+/*agregar la funcionalidad*/
+
+async function obtenerPerritoAleatorio(url){
+    const respuesta = await fetch (url)
+    const datos = await respuesta.json()
+
+    console.log(datos);
+    console.log(datos.message);
+  //  console.log(datos.status);
+
+imagenPerrito.src = datos.message
+}
+
+//obtenerPerritoAleatorio(urlAleatorios)
