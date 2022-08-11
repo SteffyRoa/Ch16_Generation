b32fbbe57ec538d112c029af64e4c955f658344d
Steffy <85137930+SteffyRoa@users.noreply.github.com>
2022-08-11 22:42:54 +0100

    avance poo js

---
+++ JS-08/clasesoop.js
@@ -0,0 +1,13 @@
+class Persona {
+
+    constructor(nombre,apellido){
+    this.nombre=nombre;
+    this.apellio = apellido;
+}
+}
+
+let persona1 = new Persona ('Jose', 'Perez');
+console.log(persona1);
+
+let persona2 = new Persona ('Karla', 'Ramirez');
+console.log(persona2);
