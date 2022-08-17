d0410a3ecb1e518ca3b42a7568eb7266204d1db8
Steffy <85137930+SteffyRoa@users.noreply.github.com>
2022-08-17 22:06:43 +0100

    avances java
    Java-variables

---
+++ JAVA/src/proyectosconJava/Variables.java
@@ -0,0 +1,19 @@
+
+package proyectosconJava;
+
+/**
+ *
+ * @author steffanyroa
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+     String nombre ="Hugo";
+     String saludo= "Hola esto es ejemplo de variables";
+     char espacio = '\u0064';
+     
+        System.out.println(saludo+ " " + espacio +" "+ nombre);  
+        
+    }
+    
+}
