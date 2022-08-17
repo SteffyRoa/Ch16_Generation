daa33f0031b988773599119313d30ebbdd007a2c
Steffy <85137930+SteffyRoa@users.noreply.github.com>
2022-08-17 22:34:26 +0100

    variables
    update

--- JAVA/src/proyectosconJava/Variables.java
+++ JAVA/src/proyectosconJava/Variables.java
@@ -8,12 +8,13 @@ package proyectosconJava;
 public class Variables {
 
     public static void main(String[] args) {
-     String nombre ="Hugo";
+     String nombre = "Hugo";
      String saludo= "Hola esto es ejemplo de variables";
      char espacio = '\u0064';
      
-        System.out.println(saludo+ " " + espacio +" "+ nombre);  
-        
+        System.out.println(saludo+ " " + espacio +" "+ nombre);
+        System.out.println("char corresponde en byte"+ Character.BYTES);
+        System.out.println("char corresponde en byte"+ Character.SIZE);
     }
     
 }
