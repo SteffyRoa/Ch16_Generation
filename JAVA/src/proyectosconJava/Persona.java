c0ce615a47c0d59e6b27a9b248cb9ccbfa274d4f
Steffy <85137930+SteffyRoa@users.noreply.github.com>
2022-08-24 23:17:14 +0100

    clase oop
    oop

---
+++ JAVA/src/proyectosconJava/Persona.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectosconJava;
+
+/**
+ *
+ * @author steffanyroa
+ */
+public class Persona {
+
+
+
+
+        String nombre = "Steffany";
+        int edad = 24;
+       int nss = 123;
+
+        void caminar() {
+            System.out.println("Caminando");
+        }
+
+
+        void dormir() {
+            System.out.println("Durmiendo");
+        }
+}
